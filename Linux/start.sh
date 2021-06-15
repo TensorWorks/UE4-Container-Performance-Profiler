ef0949ce1a818a785b04d838079933c5973ba92d
Will <will@tensorworks.com.au>
2021-06-16 08:57:55 +1000

    Add docker compose for Linux. Doesn't completely work yet, need to find the -gpus=all equivalent for docker compose

--- Linux/start.sh
+++ Linux/start.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 # Run sequencer movie
-./home/ue4/project/SubwaySequencer.sh -FullStdOutLogOutput -csvGpuStats -RenderOffScreen -Vulkan -nosound &&
+./home/ue4/project/SubwaySequencer.sh -FullStdOutLogOutput -csvGpuStats -RenderOffScreen -nosound -stdout -Vulkan &&
 # Create SVG from frame time stats
-./CSVToSVG/CSVToSVG -csvDir /home/ue4/project/SubwaySequencer/Saved/Profiling/CSV -o output/times.svg -stats FrameTime\;RenderThreadTime\;GameThreadTime\;GPUTime\;RHIThreadTime -smooth -showaverages -skipRows 100 &&
+./CSVToSVG/CSVToSVG -csvDir /home/ue4/project/SubwaySequencer/Saved/Profiling/CSV -o output/$HOSTNAME-times.svg -stats FrameTime\;RenderThreadTime\;GameThreadTime\;GPUTime\;RHIThreadTime -smooth -showaverages -skipRows 100 &&
 # Create SVG from system memory stats
-./CSVToSVG/CSVToSVG -csvDir /home/ue4/project/SubwaySequencer/Saved/Profiling/CSV -o output/memory.svg -stats MemoryFreeMB\;PhysicalUsedMB\;VirtualUsedMB -smooth -showaverages -skipRows 100 &&
-# Copy CSV to output folder
-cp -r ./home/ue4/project/SubwaySequencer/Saved/Profiling/CSV ./output
+./CSVToSVG/CSVToSVG -csvDir /home/ue4/project/SubwaySequencer/Saved/Profiling/CSV -o output/$HOSTNAME-memory.svg -stats MemoryFreeMB\;PhysicalUsedMB\;VirtualUsedMB -smooth -showaverages -skipRows 100 &&
+# Rename CSV File
+mv /home/ue4/project/SubwaySequencer/Saved/Profiling/CSV/*.csv /home/ue4/project/SubwaySequencer/Saved/Profiling/CSV/$HOSTNAME.csv
+# Copy CSV and SVGs to output folder
+cp ./home/ue4/project/SubwaySequencer/Saved/Profiling/CSV/$HOSTNAME.csv ./output
+cp -r ./svg ./output
